b9ba49307d35337ecfb033d974af9fb5dd8c74a8
evap1 <eva.poluliakhov@gmail.com>
2025-01-09 23:39:00 +0000

    revert to clean server.c without printing. clearer to work, if needed will insert back prints from last commit.

--- HW3/server.c
+++ HW3/server.c
@@ -30,7 +30,6 @@ int queue_size;
 
 int main(int argc, char *argv[])
 {
-
     int listenfd, connfd, port, clientlen, threads_num;
     struct sockaddr_in clientaddr;
 
@@ -39,7 +38,6 @@ int main(int argc, char *argv[])
     pthread_t vip_thread;
     initMaster(threads_num, queue_size, worker_threads, &vip_thread);
 
-    // segel code ----- >
     // start listening
     listenfd = Open_listenfd(port);
 
@@ -47,29 +45,18 @@ int main(int argc, char *argv[])
         clientlen = sizeof(clientaddr);
         connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
 
-        struct timeval arrival; // TO BE DELETED !!! JUST TO COMPILE FOR ENQUEUE
+        struct timeval arrival; // TO BE MODIFIED !!! JUST TO COMPILE FOR ENQUEUE
 
         pthread_mutex_lock(&lock); // -------------------------------->
 
         while (totalReqInQueue() >= queue_size){ // waiting for place in wait queue
-            
-            printf("[MASTER] Queue full! Waiting for space... (%d/%d)\n", totalReqInQueue(), queue_size);
-            struct timeval wait_start, wait_end;
-            gettimeofday(&wait_start, NULL);
-
             pthread_cond_wait(&new_req_allowed, &lock);
-
-            gettimeofday(&wait_end, NULL);
-            double wait_time = (wait_end.tv_sec - wait_start.tv_sec) + (wait_end.tv_usec - wait_start.tv_usec) / 1e6;
-            printf("[MASTER] Resumed after waiting %.6f seconds.\n", wait_time);
         }
 
         // if we are here, there is place for new req:  assign the new req to proper queue
         if(getRequestMetaData(connfd)){ // vip queue
             enqueue(vip_waiting_requests, connfd, arrival); // insert new vip req
             if (queueSize(vip_waiting_requests) == 1 ){ // if first vip req, signal there is pending vip req
-                printf("[MASTER] New VIP request! Notifying VIP thread.\n");
-
                 pthread_cond_signal(&vip_allowed);
             }
         }
@@ -82,45 +69,10 @@ int main(int argc, char *argv[])
 
         pthread_mutex_unlock(&lock); // ------------------------------^
 
-
         // 
         // HW3: In general, don't handle the request in the main thread.
         // Save the relevant info in a buffer and have one of the worker threads 
         // do the work. 
-        // was: 
-        //requestHandle(connfd);
-        // segel code ----- ^
-
-        // piazza fix (later for statistics) ----- >
-        // arrival stores when the request arrives at the server.
-        // dispatch stores when the request is assigned to a worker thread for processing.
-        // t_stats stores statistics about a specific thread.
-        // struct timeval arrival, dispatch;
-        
-        //threads_stats t_stats = (threads_stats)malloc(sizeof(struct Threads_stats));
-
-        // pupper init just for fixing the bug
-        // student code should init it properly by the thread
-        // should read about timeval and see the def of the t_stats struct
-        // Thread ID 
-        /*
-        t_stats->id = 0;
-        // Number of static requests
-        t_stats->stat_req = 0;
-        // Number of dynamic requests
-        t_stats->dynm_req = 0;
-        // Total number of requests
-        t_stats->total_req = 0;
-
-        arrival.tv_sec = 0;
-        arrival.tv_usec = 0;
-        dispatch.tv_sec = 0;
-        dispatch.tv_usec = 0;
-
-        requestHandle(connfd, arrival, dispatch, t_stats);
-        // piazza fix  ----- ^
-        Close(connfd);
-        */
     }
 }
 
@@ -149,13 +101,9 @@ void *workerThread(){
     while(1) {
         pthread_mutex_lock(&lock); // -------------------------------->
 
-        printf("[WORKER] Waiting for a request... Current Queue Size: %d\n", queueSize(waiting_requests));
-
         while(queueEmpty(waiting_requests) || !queueEmpty(vip_waiting_requests)){ // if empty or vip req, wait
-            if (!queueEmpty(vip_waiting_requests)) printf("[WORKER] VIP request is pending. Blocking worker thread.\n");
 
             pthread_cond_wait(&worker_allowed, &lock);
-            printf("[WORKER] woeker allowed, working queue: (%d/%d)\n", queueSize(waiting_requests)+ queueSize(handeling_requests), queue_size);
         }
         // if I'm here, means there is worker request and I can handle it.
         // pop from waiting, insert to handeling:
@@ -163,11 +111,9 @@ void *workerThread(){
         int connfd = dequeue(waiting_requests);
         enqueue(handeling_requests, connfd, arrival);
 
-        printf("[WORKER] Processing request %d, queue size: %d\n", connfd, queueSize(handeling_requests));
-
         pthread_mutex_unlock(&lock); // ------------------------------^
 
-        struct timeval dispatch; // TO BE DELETED !!! JUST TO COMPILE FOR ENQUEUE
+        struct timeval dispatch; // TO BE MODIFIEF !!! JUST TO COMPILE FOR ENQUEUE
         threads_stats t_stats = (threads_stats)malloc(sizeof(struct Threads_stats));
 
         // pupper init just for fixing the bug
@@ -205,8 +151,6 @@ void *vipThread(){
     while(1){
         pthread_mutex_lock(&lock); // -------------------------------->
         
-        printf("[VIP] Waiting for VIP request... Current Queue Size: %d\n", queueSize(vip_waiting_requests));
-
         while(queueEmpty(vip_waiting_requests)){ // if no vip req, wait
             pthread_cond_wait(&vip_allowed, &lock);
         }
@@ -214,11 +158,9 @@ void *vipThread(){
         struct timeval arrival = queueHeadArrivalTime(vip_waiting_requests);
         int connfd = headDesciprot(vip_waiting_requests);
 
-        printf("[VIP] Processing VIP request %d, queue size: %d\n", connfd, queueSize(vip_waiting_requests));
-
         pthread_mutex_unlock(&lock); // ------------------------------^
 
-        struct timeval dispatch; // TO BE DELETED !!! JUST TO COMPILE FOR ENQUEUE
+        struct timeval dispatch; // TO BE MODIFIED !!! JUST TO COMPILE FOR ENQUEUE
         threads_stats t_stats = (threads_stats)malloc(sizeof(struct Threads_stats));
 
         // pupper init just for fixing the bug
