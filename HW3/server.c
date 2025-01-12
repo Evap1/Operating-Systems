18125a7ebf8e55328303eff7b7d1bbf4dd41ef4a
evap1 <eva.poluliakhov@gmail.com>
2025-01-12 09:56:22 +0000

    1. Deleted skip for vip as described in new PDF 2. Implemented overload handling 3. Added skip req to handling after removing from waiting 4. Verified close of fd after duque TODO: MAKE SURE SIGNALS ARE SENT ACCORDING TO POLICY
    Co-authored-by: Batel Divinsky <DvBatel@users.noreply.github.com>

--- HW3/server.c
+++ HW3/server.c
@@ -2,7 +2,9 @@
     #include "request.h"
     #include "queue.h"
     #include "pthread.h"
-
+    #define BUFFER 10
+    #define NUM_OF_POLICIES 5
+    
     //
     // server.c: A very, very simple web server
     //
@@ -12,27 +14,32 @@
     // Repeatedly handles HTTP requests sent to this port number.
     // Most of the work is done within routines written in request.c
     //
+    int DEBUG = 0;
 
     // DECLERATIONS
-    void initMaster();
+    void initMaster(int threads_num, int queue_size, pthread_t *worker_threads, pthread_t* vip_thread);
     void *workerThread();
     void *vipThread();
-    void getargs(int *port, int *threads_num, int *queue_size, int argc, char *argv[]);
+    void getargs(int *port, int *threads_num, int *queue_size, char* policy, int argc, char *argv[]);
     int totalReqInQueue();
+    bool isValidPolicy(char* policy);
 
     // GLOBALS
     Queue handeling_requests, waiting_requests, vip_waiting_requests;
     pthread_mutex_t lock;
-    pthread_cond_t new_req_allowed, vip_allowed, worker_allowed;
+    pthread_cond_t new_req_allowed, vip_allowed, worker_allowed, empty_queue, full_queue;
     int queue_size;
     int handeling_vip;
+    const char* common_policies[] = {"block", "dh", "dt", "bf", "random"};
 
+    
     int main(int argc, char *argv[])
     {
         int listenfd, connfd, port, clientlen, threads_num;
+        char policy[BUFFER];
         struct sockaddr_in clientaddr;
 
-        getargs(&port, &threads_num, &queue_size, argc, argv);
+        getargs(&port, &threads_num, &queue_size, policy, argc, argv);
         pthread_t *worker_threads = malloc(sizeof(pthread_t) * threads_num);
         pthread_t vip_thread;
         initMaster(threads_num, queue_size, worker_threads, &vip_thread);
@@ -48,10 +55,50 @@
             struct timeval arrival; // TO BE MODIFIED !!! JUST TO COMPILE FOR ENQUEUE
 
             pthread_mutex_lock(&lock); // -------------------------------->
+            
+            //proccess full queue according to policy
+            if (totalReqInQueue() >= queue_size) {
+                if ((sizeQueue(vip_waiting_requests) == queue_size) || strcmp(policy,"block") == 0) {   //as it wassssssss
+                    while (totalReqInQueue() >= queue_size)
+                    { // waiting for place in wait queue
+                        pthread_cond_wait(&new_req_allowed, &lock);
+                    }
+                }
+                else if (strcmp(policy,"dt") == 0) {                //drop the request and leaveeeee
+                    Close(connfd);
+                    pthread_mutex_unlock(&lock);
+                    continue;
+                }
+                else if (strcmp(policy,"dh") == 0) {                //drop latest request in queue
+                    int fd_to_delete = dequeue(waiting_requests);
+                    if (fd_to_delete != -1) {
+                    Close(fd_to_delete); 
+                    }
+                }
+                else if (strcmp(policy,"bf") == 0) {                //NOT A BUSY WAIT
+                    while (totalReqInQueue() != 0)                  //use a brand new spanking conditional variable to wait till queue is empty
+                    { // waiting for place in wait queue
+                        pthread_cond_wait(&empty_queue, &lock);
+                    }
+                    if(!getRequestMetaData(connfd)){                // if req A is vip it shouldnt be droped.
+                        Close(connfd);                              // after empty drop worker hoe req
+                        pthread_mutex_unlock(&lock);
+                        continue;
+                    }
+                }
+                else if (strcmp(policy,"random") == 0) {            //drop 50% of the waiting requests by random
 
-            while (totalReqInQueue() >= queue_size)
-            { // waiting for place in wait queue
-                pthread_cond_wait(&new_req_allowed, &lock);
+                    int num_of_waiting_req = queueSize(waiting_requests);
+                    int i = num_of_waiting_req;
+
+                    while (num_of_waiting_req <= i / 2) {
+                        int j = rand() % (num_of_waiting_req);  //generate a number between 0 and the total wait queue size
+
+                        int fd_to_delete = dequeueByIndex(waiting_requests, j);
+                        Close(fd_to_delete);
+                        num_of_waiting_req--;
+                    }
+                }
             }
 
             // if we are here, there is place for new req:  assign the new req to proper queue
@@ -83,7 +130,7 @@
 
     // connfd : connection file descriptor, represents the new client connection that the server has accepted
 
-    void initMaster(int threads_num, int queue_size, pthread_t *worker_threads, pthread_t vip_thread)
+    void initMaster(int threads_num, int queue_size, pthread_t *worker_threads, pthread_t* vip_thread)
     {
         pthread_mutex_init(&lock, NULL);
         waiting_requests = queueCreate(queue_size);
@@ -92,13 +139,15 @@
         pthread_cond_init(&new_req_allowed, NULL);
         pthread_cond_init(&worker_allowed, NULL);
         pthread_cond_init(&vip_allowed, NULL);
+        pthread_cond_init(&empty_queue, NULL);
+        pthread_cond_init(&full_queue, NULL);
         handeling_vip = 0;
         pthread_mutex_lock(&lock); // -------------------------------->
         for (int i = 0; i < threads_num; i++)
         {
             pthread_create(&worker_threads[i], NULL, workerThread, NULL);
         }
-        pthread_create(&vip_thread, NULL, vipThread, NULL);
+        pthread_create(vip_thread, NULL, vipThread, NULL);
         pthread_mutex_unlock(&lock); // ------------------------------^
     }
 
@@ -108,7 +157,6 @@
         while (1)
         {
             pthread_mutex_lock(&lock); // -------------------------------->
-
             while (queueEmpty(waiting_requests) || !queueEmpty(vip_waiting_requests) || handeling_vip)
             { // if empty or vip req, wait
                 pthread_cond_wait(&worker_allowed, &lock);
@@ -154,6 +202,9 @@
                 if (skip_connfd == -1) { //no new fd found
                     skip_invoked = 0;
                 }
+                else {
+                    enqueue(handeling_requests, skip_connfd, arrival);
+                }
                 // do we want to change the fd of ourselves or does it not mean anything?
                 // if error accures, notice handling_queue
             }
@@ -164,11 +215,18 @@
                 Close(skip_connfd);
             }
 
-            pthread_mutex_lock(&lock); // -------------------------------->
+            pthread_mutex_lock(&lock); // --------------------------------> 
             dequeueByReq(handeling_requests, connfd);
+            if (skip_invoked){
+                dequeueByReq(handeling_requests, skip_connfd);
+            }
             if (totalReqInQueue() < queue_size)
             { // this req is done. get another one by master
                 pthread_cond_signal(&new_req_allowed);
+            }
+            if (totalReqInQueue() == 0)
+            { // for block_flush
+                pthread_cond_signal(&empty_queue);
             }
             pthread_mutex_unlock(&lock); // ------------------------------^
         }
@@ -212,25 +270,8 @@
             dispatch.tv_sec = 0;
             dispatch.tv_usec = 0;
             // TO BE DELETED
-            int skip_invoked = requestHandle(connfd, arrival, dispatch, t_stats); // perfrom request outside of lock
+            requestHandle(connfd, arrival, dispatch, t_stats); // perfrom request outside of lock
             Close(connfd);
-
-            pthread_mutex_lock(&lock); // -------------------------------->
-            int skip_connfd;
-            if(skip_invoked) { // need to handle latest req by the sme thread
-                arrival = queueHeadArrivalTime(vip_waiting_requests); // to be changed
-                skip_connfd = skipDequeue(vip_waiting_requests);
-                if (skip_connfd == -1) { //no new fd found
-                    skip_invoked = 0;
-                }
-                // if error accures, notice handling_queue
-            }
-            pthread_mutex_unlock(&lock); // ------------------------------^
-
-            if(skip_invoked) {      //not supposed to check reccursive skip
-                requestHandle(skip_connfd, arrival, dispatch, t_stats);
-                Close(skip_connfd);
-            }
 
             pthread_mutex_lock(&lock); // -------------------------------->
             handeling_vip = 0;
@@ -242,13 +283,26 @@
             { // this req is done. get another one by master
                 pthread_cond_signal(&new_req_allowed);
             }
+            if (totalReqInQueue() == 0)
+            { // for block_flush
+                pthread_cond_signal(&empty_queue);
+            }
             pthread_mutex_unlock(&lock); // ------------------------------^
         }
     }
 
-    void getargs(int *port, int *threads_num, int *queue_size, int argc, char *argv[])
+    bool isValidPolicy(char* policy) {
+        for(int i = 0; i < NUM_OF_POLICIES; i++) {
+            if (strcmp(common_policies[i], policy) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void getargs(int *port, int *threads_num, int *queue_size, char* policy, int argc, char *argv[])
     {
-        if (argc < 4)
+        if (argc < 5)
         {
             fprintf(stderr, "Usage: %s <port>\n", argv[0]);
             exit(1);
@@ -257,10 +311,11 @@
         *threads_num = atoi(argv[2]);
         *queue_size = atoi(argv[3]);
 
-        if (*port <= 1024 || *threads_num <= 0 || *queue_size <= 0)
+        if (*port <= 1024 || *threads_num <= 0 || *queue_size <= 0 || !isValidPolicy(argv[4]))
         { // argument validation
             exit(1);
         }
+        strcpy(policy, argv[4]);
     }
 
     // just to make code more readable
@@ -268,3 +323,10 @@
     {
         return queueSize(vip_waiting_requests) + queueSize(waiting_requests) + queueSize(handeling_requests) + handeling_vip;
     }
+
+// return number of waiting requests
+    int totalWaitReqInQueue()
+    {
+        return queueSize(vip_waiting_requests) + queueSize(waiting_requests);
+    }
+
